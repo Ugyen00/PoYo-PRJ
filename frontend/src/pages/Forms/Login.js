37f8c3c0608d61a72612e6ce9c8ee1164d09dd8e
unknown <yoedsel@example.com>
2024-04-15 20:28:47 +0600

    nn

--- frontend/src/pages/Forms/Login.js
+++ frontend/src/pages/Forms/Login.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import NavBar from '../../components/NavBar'
-import Footer from '../../components/Footer'
+// import NavBar from '../../components/NavBar'
 
 const Login = () => {
     return (
